9f3d043406a7c57c236c7d3c0443fc07e63156bf
André de Fazzio Posman <andre.posman@gmail.com>
2021-09-18 22:40:51 -0300

    WIP

--- main.go
+++ main.go
@@ -7,23 +7,7 @@ import (
 
 
 func main() {
-	/*
-	TODO:
-		- add tests
-		- validate requirements
-		- add swagger
-		- better data manipulation
-		- change lat/lng to strings
-		- docker/docker-compose
-		- review solid/12factors/clean code principles
-
-	TODO improvements:
-		- host cloud
-		- add db
-		- monitor/observability tools
-
-	*/
-
 	banner.GreetingBanner()
+
 	api.InitAPI()
 }
